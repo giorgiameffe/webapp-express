bd8e3e3075a15ca263bbfb5de748fc4cf8d56014
giorgiameffe <giorgiameffe03@gmail.com>
2025-05-09 16:32:46 +0200

    feat: create moviesRouter and import it on app

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // express
 const express = require('express');
-
 const app = express();
 const port = 3000;
+
+// routers
+const moviesRouter = require('./routers/movies.js');
 
 app.get('/', (req, res) => {
     res.send('Welcome to my Web App');
 })
+
+// movies
+app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
     console.log(`Server listening at port ${port}`);
---
+++ routers/movies.js
@@ -0,0 +1,14 @@
+// express
+const express = require('express');
+const router = express.Router();
+
+// controller
+const movieController = require('../controllers/movieController.js');
+
+// index
+router.get('/', movieController.index);
+
+// show
+router.get('/:id', movieController.show);
+
+module.exports = router;
