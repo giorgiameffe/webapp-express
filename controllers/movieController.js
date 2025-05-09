f04a4db2d41a178939ed071bea48de4707bc9fb3
giorgiameffe <giorgiameffe03@gmail.com>
2025-05-09 18:18:08 +0200

    feat: add show route

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -7,21 +7,26 @@ function index(req, res) {
     const sql = 'SELECT * FROM movies';
 
     connection.query(sql, (err, results) => {
-        if (err) {
-
-            return res.status(500).json({
-                error: 'Database connection failed'
-            })
-        }
-
+        if (err) return res.status(500).json({ error: 'Database connection failed' })
         res.json(results);
-    })
 
+    })
 }
 
 // show
 function show(req, res) {
-    res.send('Show single movie');
+
+    // recuperare id
+    const id = parseInt(req.params.id);
+
+    // salvare in una variabile la query da utilizzare
+    const sql = 'SELECT * FROM movies WHERE id = ?';
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database connection failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Movie not found' });
+        res.json(results[0]);
+    })
 }
 
 module.exports = {
