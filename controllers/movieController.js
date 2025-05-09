375072895cba0a7d3011d6b8d1a2911d2467c95a
giorgiameffe <giorgiameffe03@gmail.com>
2025-05-09 17:47:24 +0200

    feat: add index route

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,7 +3,20 @@ const connection = require('../data/db.js');
 
 // index
 function index(req, res) {
-    res.send('Show all movies');
+
+    const sql = 'SELECT * FROM movies';
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+
+            return res.status(500).json({
+                error: 'Database connection failed'
+            })
+        }
+
+        res.json(results);
+    })
+
 }
 
 // show
